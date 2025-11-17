fe3adfbc2d05bfb2c893f26b43b5984bc60e5f1a
hicham altit <hichamaltit91@gmail.com>
2025-11-17 16:23:30 +0100

    fix

--- resources/views/components/layout/notification-list.blade.php
+++ resources/views/components/layout/notification-list.blade.php
@@ -53,7 +53,7 @@
                         <svg class="h-3.5 w-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                         </svg>
-                        <span x-text="formatTime(notification.created_at)"></span>
+                        <span x-text="formatDate(notification.created_at)"></span>
                     </div>
                 </div>
             </div>
